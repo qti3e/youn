af9b6705da315253f4e0c06026096f7bdf4f22c6
Qti3e <Alirezaghadimy@gmail.com>
2016-07-20 20:07:36 +0430

    First version of errors  template

---
+++ core/exception/template.php
@@ -0,0 +1,62 @@
+<?php
+/*****************************************************************************
+ *         In the name of God the Most Beneficent the Most Merciful          *
+ *___________________________________________________________________________*
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 3 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *___________________________________________________________________________*
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   GNU General Public License for more details.                            *
+ *___________________________________________________________________________*
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *___________________________________________________________________________*
+ *                             Created by  Qti3e                             *
+ *        <http://Qti3e.Github.io>    LO-VE    <Qti3eQti3e@Gmail.com>        *
+ *****************************************************************************/
+?>
+<html>
+	<head>
+		<title>Internal error</title>
+		<style type="text/css">
+			html{
+				height: 100%;
+				width: 100%;
+				background-color: #cccccc;
+			}
+			body{
+				width: 700px;
+				height: 500px;
+				background-color: #ffffff;
+				box-shadow: #000 0px 10px 20px;
+				display: block;
+				margin-right: auto;
+				margin-left: auto;
+				padding: 5px;
+			}
+			.error{
+				background-color: #ff2020;
+				color: #ffffff;
+			}
+		</style>
+	</head>
+	<body>
+		<h1>Internal Error!</h1>
+		<hr>
+		<p>
+			<?php echo $errorStr; ?>
+		</p>
+		<address>File: <?php echo $errorFile; ?> & Line <?php echo $errorLine ?></address>
+		<code>
+			<?php echo ($errorLine-1).substr(str_replace('&lt;?php&nbsp;','',highlight_string('<?php '.$lines[$errorLine-2],true)),6,-7); ?>
+			<b class="error">
+				<?php echo $errorLine.substr(str_replace('&lt;?php&nbsp;','',highlight_string('<?php '.$line,true)),6,-7); ?>
+			</b>
+			<?php echo ($errorLine+1).substr(str_replace('&lt;?php&nbsp;','',highlight_string('<?php '.$lines[$errorLine],true)),6,-7); ?>
+		</code>
+	</body>
+</html>
