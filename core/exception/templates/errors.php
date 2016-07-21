901485e8ea744002d4b00dcdab117fc70e1c7598
Qti3e <Alirezaghadimy@gmail.com>
2016-07-21 10:00:34 +0430

    Add display error template

---
+++ core/exception/templates/errors.php
@@ -0,0 +1,50 @@
+<?php
+/*****************************************************************************
+ *         In the name of God the Most Beneficent the Most Merciful          *
+ *___________________________________________________________________________*
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 3 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *___________________________________________________________________________*
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   GNU General Public License for more details.                            *
+ *___________________________________________________________________________*
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *___________________________________________________________________________*
+ *                             Created by  Qti3e                             *
+ *        <http://Qti3e.Github.io>    LO-VE    <Qti3eQti3e@Gmail.com>        *
+ *****************************************************************************/
+?>
+<html>
+<head>
+	<title><?php echo $header; ?></title>
+	<style type="text/css">
+		html{
+			height: 100%;
+			width: 100%;
+			background-color: #cccccc;
+		}
+		body{
+			width: 700px;
+			height: 500px;
+			background-color: #ffffff;
+			box-shadow: #000 0px 10px 20px;
+			display: block;
+			margin-right: auto;
+			margin-left: auto;
+			padding: 5px;
+		}
+	</style>
+</head>
+<body>
+<h1><?php echo $header ?></h1>
+<hr>
+<p>
+	<?php echo $message; ?>
+</p>
+</body>
+</html>
