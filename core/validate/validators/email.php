58c821a474b382c7e23aee5d9c5ab75acf5dc88a
Qti3e <Alirezaghadimy@gmail.com>
2016-07-24 19:02:00 +0430

    Add validator library

--- core/validate/validators/email.php
+++ core/validate/validators/email.php
@@ -19,9 +19,11 @@
  *        <http://Qti3e.Github.io>    LO-VE    <Qti3eQti3e@Gmail.com>        *
  *****************************************************************************/
 
-namespace core\helper;
+namespace core\validate\validators;
 
 
-class validator {
+use core\validate\validatorInterface;
+
+class email implements validatorInterface{
 
 }
