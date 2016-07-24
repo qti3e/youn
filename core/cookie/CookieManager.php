bba31f32a168c524229ec259b09d94abe98ded04
Qti3e <Alirezaghadimy@gmail.com>
2016-07-24 19:01:32 +0430

    Add cookie manager exception

--- core/cookie/CookieManager.php
+++ core/cookie/CookieManager.php
@@ -21,7 +21,215 @@
 
 namespace core\cookie;
 
+use core\database\KeyValueStore;
 
-class CookieManager {
+/**
+ * Class CookieManager
+ * Session manager
+ * @package core\cookie
+ */
+class CookieManager{
+	/**
+	 * @var null
+	 */
+	protected static $path      = null;
+	/**
+	 * @var null
+	 */
+	protected static $domain    = null;
+	/**
+	 * @var null
+	 */
+	protected static $secure    = null;
+	/**
+	 * @var null
+	 */
+	protected static $httponly  = null;
+	/**
+	 * @var null
+	 */
+	protected static $expire    = null;
 
+	/**
+	 * @param      $name
+	 * @param      $value
+	 * @param null $expire
+	 * @param null $path
+	 * @param null $domain
+	 * @param null $secure
+	 * @param null $httponly
+	 *
+	 * @return bool
+	 */
+	public static function set($name,$value,$expire = null,$path = null,$domain = null,$secure = null,$httponly = null){
+		$name   = cryptography::encrypt($name);
+		$value  = cryptography::encrypt($value);
+		if($expire === null){
+			if(static::$expire === null){
+				//Set default value to one month
+				static::$expire   = time() + (30 * 24 * 60 * 60);
+			}
+			$expire = static::$expire;
+		}
+		if($path    === null){
+			$path   = static::$path;
+		}
+		if($domain  === null){
+			$domain = static::$domain;
+		}
+		if($secure  === null){
+			$secure = static::$secure;
+		}
+		if($httponly    === null){
+			$httponly   = static::$httponly;
+		}
+		return setcookie($name,$value,$expire,$path,$domain,$secure,$httponly);
+	}
+
+	/**
+	 * @param $name
+	 *
+	 * @return bool
+	 */
+	public static function get($name){
+		$name   = cryptography::encrypt($name);
+		if(isset($_COOKIE[$name])){
+			return cryptography::decrypt($_COOKIE[$name]);
+		}
+		return false;
+	}
+
+	/**
+	 * @return null
+	 */
+	public static function neverDie(){
+		//Set cookie for 20 years!, so it'll never die
+		return static::setExpire(time() + (20 * 365 * 24 * 60 * 60));
+	}
+
+	/**
+	 * Return timestamp for 20 years later.
+	 * @return int
+	 */
+	public static function createNeverDieTime(){
+		return time() + (20 * 365 * 24 * 60 * 60);
+	}
+
+	/**
+	 * @param      $name
+	 * @param      $value
+	 * @param null $expire
+	 * @param null $path
+	 * @param null $domain
+	 * @param null $secure
+	 * @param null $httponly
+	 *
+	 * @return bool
+	 */
+	public static function SetIfNotExists($name,$value,$expire = null,$path = null,$domain = null,$secure = null,$httponly = null){
+		if(static::isDefined($name)){
+			return false;
+		}
+		return static::set($name,$value,$expire,$path,$domain,$secure,$httponly);
+	}
+
+	/**
+	 * @param $name
+	 *
+	 * @return bool
+	 */
+	public static function isDefined($name){
+		$name   = cryptography::encrypt($name);
+		return isset($_COOKIE[$name]);
+	}
+
+	/**
+	 * @return null
+	 */
+	public static function getDomain() {
+		return static::$domain;
+	}
+
+	/**
+	 * @return null
+	 */
+	public static function getExpire() {
+		return static::$expire;
+	}
+
+	/**
+	 * @return null
+	 */
+	public static function getHttponly() {
+		return static::$httponly;
+	}
+
+	/**
+	 * @return null
+	 */
+	public static function getPath() {
+		return static::$path;
+	}
+
+	/**
+	 * @return null
+	 */
+	public static function getSecure() {
+		return static::$secure;
+	}
+
+	/**
+	 * @param $domain
+	 *
+	 * @return null
+	 */
+	public static function setDomain($domain) {
+		$re = static::$domain;
+		static::$domain = $domain;
+		return $re;
+	}
+
+	/**
+	 * @param $expire
+	 *
+	 * @return null
+	 */
+	public static function setExpire($expire) {
+		$re = static::$expire;
+		static::$expire = $expire;
+		return $re;
+	}
+
+	/**
+	 * @param $httponly
+	 *
+	 * @return null
+	 */
+	public static function setHttponly($httponly) {
+		$re = static::$httponly;
+		static::$httponly = $httponly;
+		return $re;
+	}
+
+	/**
+	 * @param $path
+	 *
+	 * @return null
+	 */
+	public static function setPath($path) {
+		$re = static::$path;
+		static::$path = $path;
+		return $re;
+	}
+
+	/**
+	 * @param $secure
+	 *
+	 * @return mixed
+	 */
+	public static function setSecure($secure) {
+		$re = $secure;
+		static::$secure = $secure;
+		return $re;
+	}
 }
---
+++ core/cookie/cryptography.php
@@ -0,0 +1,141 @@
+<?php
+/*****************************************************************************
+ *         In the name of God the Most Beneficent the Most Merciful          *
+ *___________________________________________________________________________*
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 3 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *___________________________________________________________________________*
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   GNU General Public License for more details.                            *
+ *___________________________________________________________________________*
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *___________________________________________________________________________*
+ *                             Created by  Qti3e                             *
+ *        <http://Qti3e.Github.io>    LO-VE    <Qti3eQti3e@Gmail.com>        *
+ *****************************************************************************/
+
+namespace core\cookie;
+
+/**
+ * Class cryptography
+ *  Encrypt and decrypt cookies.
+ * @package core\cookie
+ */
+class cryptography {
+	/**
+	 * @var string
+	 */
+	protected static $key       = '';
+	/**
+	 * @var int
+	 */
+	protected static $len       = 0;
+	/**
+	 * @var bool
+	 */
+	protected static $on        = false;
+	/**
+	 * @return string
+	 */
+	public static function getKey() {
+		return static::$key;
+	}
+
+	/**
+	 * @param $key
+	 *
+	 * @return void
+	 */
+	public static function setKey($key) {
+		static::$key  = $key;
+		static::$len  = strlen($key);
+	}
+
+	/**
+	 * @return bool
+	 */
+	public static function isOn() {
+		return static::$on;
+	}
+
+	/**
+	 * @return void
+	 */
+	public static function turnOn() {
+		static::$on   = true;
+	}
+
+	/**
+	 * @return void
+	 */
+	public static function turnOff(){
+		static::$on   = false;
+	}
+
+	/**
+	 * @param int $len
+	 *
+	 * @return string
+	 */
+	public static function generate_key($len = 256){
+		$re         = '';
+		$alphabets  = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=';
+		$aLen       = strlen($alphabets) - 1;
+		for($i = 0;$i < $len;$i++){
+			$re .= $alphabets[rand(0,$aLen * 3) % $aLen];
+		}
+		return $re;
+	}
+
+	/**
+	 * @param $n
+	 *
+	 * @return int
+	 */
+	protected static function getJ($n){
+		$j  = $n % static::$len;
+		if($n == 0){
+			return ord(static::$key[$j])+ord(static::$key[static::$len - $j - 1]);
+		}
+		$p  = ($n-1) % static::$len;
+		return ord(static::$key[$j])+ord(static::$key[static::$len - $j - 1])+ord(static::$key[$p])+ord(static::$key[static::$len - $p - 1]);
+	}
+	/**
+	 * @param string $string
+	 *
+	 * @return string
+	 */
+	public static function encrypt($string){
+		if(!static::$on){
+			return $string;
+		}
+		$StringLen  = strlen($string);
+		for($i = 0;$i < $StringLen;$i++){
+			$j  = static::getJ($i);
+			$string[$i] = chr((ord($string[$i])+$j) % 256);
+		}
+		return $string;
+	}
+
+	/**
+	 * @param string $string
+	 *
+	 * @return string
+	 */
+	public static function decrypt($string){
+		if(!static::$on){
+			return $string;
+		}
+		$StringLen  = strlen($string);
+		for($i = 0;$i < $StringLen;$i++){
+			$j  = static::getJ($i);
+			$string[$i] = chr((ord($string[$i]) - $j) % 256);
+		}
+		return $string;
+	}
+}
