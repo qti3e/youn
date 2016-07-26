b895c02d219c8f28f4bc1484b6462b0baef4aed3
Qti3e <Alirezaghadimy@gmail.com>
2016-07-26 21:45:16 +0430

    debug getUserIP()

--- core/http/http.php
+++ core/http/http.php
@@ -35,7 +35,9 @@ class http {
 			return $_SERVER['HTTP_CLIENT_IP'];
 		}elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
 			return $_SERVER['HTTP_X_FORWARDED_FOR'];
+		}elseif(isset($_SERVER['REMOTE_ADDR'])){
+			return $_SERVER['REMOTE_ADDR'];
 		}
-		return $_SERVER['REMOTE_ADDR'];
+		return false;
 	}
 }
