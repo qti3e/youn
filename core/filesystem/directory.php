f7dd362131b43befb1960c318cb5874b2409d8a8
Qti3e <Alirezaghadimy@gmail.com>
2016-08-02 17:19:21 +0430

    Write directory library

--- core/filesystem/directory.php
+++ core/filesystem/directory.php
@@ -27,93 +27,178 @@ namespace core\filesystem;
  */
 class directory {
 	/**
-	 * @param      $directory
-	 * @param null $depth
-	 *
-	 * @return void
+	 * @var \Exception
 	 */
-	public static function map($directory,$depth = null){
+	protected static $error;
 
+	/**
+	 * @return mixed
+	 */
+	public static function getLastError(){
+		return static::$error;
 	}
 
 	/**
 	 * @param $dir
 	 *
-	 * @return void
+	 * @return bool
 	 */
 	public static function exists($dir){
-
+		return (file_exists($dir) && is_dir($dir));
 	}
 
 	/**
 	 * @param $dir
+	 * @param $mode
 	 *
-	 * @return void
+	 * @return bool
 	 */
-	public static function mk($dir){
-
+	public static function mk($dir,$mode = '0777'){
+		return mkdir($dir,$mode);
 	}
 
 	/**
 	 * @param $dir
 	 *
-	 * @return void
+	 * @return bool
 	 */
 	public static function rm($dir){
-
-	}
-
-	/**
-	 * @param $dir
-	 * @param $path
-	 *
-	 * @return void
-	 */
-	public static function copy($dir,$path){
-
-	}
-
-	/**
-	 * @param $dir
-	 * @param $path
-	 *
-	 * @return void
-	 */
-	public static function cut($dir,$path){
-
+		return rmdir($dir);
 	}
 
 	/**
 	 * @param $dir
 	 *
-	 * @return void
+	 * @return bool
 	 */
 	public static function clear($dir){
-
+		$files  = glob($dir.'/*');
+		$keys   = array_keys($files);
+		$count  = count($files);
+		for($i  = 0;$i < $count;$i++){
+		    $file   = $files[$keys[$i]];
+			if(is_file($file) || is_link($file)){
+				if(!unlink($file)){
+					return false;
+				};
+			}else{
+				if(!static::clear($file)){
+					return false;
+				};
+				if(!rmdir($file)){
+					return false;
+				};
+			}
+		}
+		return true;
 	}
 
 	/**
 	 * @param $dir
 	 *
-	 * @return void
+	 * @return bool
+	 */
+	public static function clearDirs($dir){
+		$files  = glob($dir.'/*');
+		$keys   = array_keys($files);
+		$count  = count($files);
+		for($i  = 0;$i < $count;$i++){
+			$dir= $files[$keys[$i]];
+			if(is_dir($dir)){
+				if(!static::clear($dir)){
+					return false;
+				};
+				if(!rmdir($dir)){
+					return false;
+				};
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * @param $dir
+	 *
+	 * @return bool
+	 */
+	public static function clearFiles($dir){
+		$files  = glob($dir.'/*');
+		$keys   = array_keys($files);
+		$count  = count($files);
+		for($i  = 0;$i < $count;$i++){
+			$file   = $files[$keys[$i]];
+			if(is_file($file) || is_link($file)){
+				if(!unlink($dir)){
+					return false;
+				};
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * @param $dir
+	 *
+	 * @return bool|array
 	 */
 	public static function getFilesList($dir){
-
+		if(($list = static::getContentList($dir)) === false){
+			return false;
+		}
+		return $list['files'];
 	}
 
 	/**
 	 * @param $dir
 	 *
-	 * @return void
+	 * @return bool|array
 	 */
 	public static function getDirsList($dir){
-
+		if(($list = static::getContentList($dir)) === false){
+			return false;
+		}
+		return $list['dirs'];
 	}
 
 	/**
-	 * @return void
+	 * @param $dir
+	 * @return bool|array
 	 */
-	public static function getContentList() {
-
+	public static function getContentList($dir) {
+		try{
+			$dirs   = [];
+			$files  = [];
+			$d  = dir($dir);
+			while(false !== ($entry = $d->read())){
+				if(is_dir($entry)){
+					$dirs[]     = $entry;
+				}else{
+					$files[]    = $entry;
+				}
+			}
+			sort($dirs);
+			sort($files);
+			$d->close();
+			return [
+				'dirs'  => $dirs,
+				'files' => $files
+			];
+		}catch (\Exception $e){
+			static::$error  = $e;
+			return false;
+		}
 	}
+
+	/**
+	 * @param $dir
+	 * @return bool
+	 */
+	public static function ch($dir){
+		return chdir($dir);
+	}
+
+//TODO:
+//	public static function copy($dir,$path){}
+//	public static function cut($dir,$path){}
+//	public static function map($directory,$depth = null){}
 }
