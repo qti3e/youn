a64c09805a6d0e4ed3b4db360948a1a8562a09cf
Qti3e <Alirezaghadimy@gmail.com>
2016-08-03 14:11:56 +0430

    Write result manager for database library

---
+++ core/database/result.php
@@ -0,0 +1,75 @@
+<?php
+/*****************************************************************************
+ *         In the name of God the Most Beneficent the Most Merciful          *
+ *___________________________________________________________________________*
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 3 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *___________________________________________________________________________*
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   GNU General Public License for more details.                            *
+ *___________________________________________________________________________*
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *___________________________________________________________________________*
+ *                             Created by  Qti3e                             *
+ *        <http://Qti3e.Github.io>    LO-VE    <Qti3eQti3e@Gmail.com>        *
+ *****************************************************************************/
+
+namespace core\database;
+
+/**
+ * Class result
+ * @package core\database
+ */
+class result {
+	/**
+	 * @var
+	 */
+	private $result;
+	/**
+	 * @var mixed
+	 */
+	public $num_rows;
+
+	/**
+	 * result constructor.
+	 *
+	 * @param $result
+	 */
+	public function __construct($result){
+		$this->result   = $result;
+		$this->num_rows = query::getObj()->num_rows($result);
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function num_rows(){
+		return query::getObj()->num_rows($this->result);
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function fetch_assoc() {
+		return query::getObj()->fetch_assoc($this->result);
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function fetch_array(){
+		return query::getObj()->fetch_array($this->result);
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function fetch_all(){
+		return query::getObj()->fetch_all($this->result);
+	}
+}
